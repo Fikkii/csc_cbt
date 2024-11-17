b3946d3359396a25245f000ac8cc938e68749394
fikki <ajalafikayo@gmail.com>
2024-11-17 10:02:35 +0100

    redesign and working cbt closer to completion

--- db.php
+++ db.php
@@ -1,20 +1,20 @@
 <?php
 function connect(){
 
-$dbconfig = [
-    'username'=> 'root',
-    'password'=> '',
-    'host'=> 'localhost',
-    'database'=> 'csc'
-];
-
-
 //$dbconfig = [
-//    'username'=> 'm4303_fikki',
-//    'password'=> 'Bunmisegun"2016',
-//    'host'=> 'mysql6.serv00.com',
-//    'database'=> 'm4303_csc'
+//    'username'=> 'root',
+//    'password'=> '',
+//    'host'=> 'localhost',
+//    'database'=> 'csc'
 //];
+
+
+$dbconfig = [
+    'username'=> 'm4303_fikki',
+    'password'=> 'Bunmisegun"2016',
+    'host'=> 'mysql6.serv00.com',
+    'database'=> 'm4303_csc'
+];
 
     $db = new mysqli($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
 
